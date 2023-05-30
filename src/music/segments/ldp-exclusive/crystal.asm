fc88e179c38284e685fbcaee5db75a343059cea8
TempoQuill <jicrapo@gmail.com>
2023-05-30 06:57:54 -0400

    Add more LDP material

---
+++ src/music/segments/ldp-exclusive/crystal.asm
@@ -0,0 +1,43 @@
+MusicDataCrystal:
+MusicDataCrystal_Square2:
+	.db $99
+	.db $40
+	.db $94
+	.db $40
+	.db $98
+	.db $40
+	.db $98
+	.db $56
+	.db $56
+	.db $52
+	.db $9B
+	.db $50
+	.db $00
+MusicDataCrystal_Square1:
+	.db $99
+	.db $36
+	.db $94
+	.db $36
+	.db $98
+	.db $36
+	.db $98
+	.db $44
+	.db $98
+	.db $44
+	.db $98
+	.db $40
+	.db $9B
+	.db $3E
+MusicDataCrystal_Triangle:
+	.db $89
+	.db $40
+	.db $84
+	.db $40
+	.db $88
+	.db $40
+	.db $88
+	.db $4E
+	.db $4E
+	.db $4A
+	.db $8B
+	.db $48
---
+++ src/music/segments/ldp-exclusive/subspace.asm
@@ -0,0 +1,44 @@
+MusicDataSubSpace:
+MusicDataSubSpace_Square2:
+	.db $80
+	.db $7e
+	.db $84
+	.db $50
+	.db $58
+	.db $5a
+	.db $88
+	.db $5e
+	.db $84
+	.db $5a
+	.db $58
+	.db $52
+	.db $50
+	.db $52
+	.db $4e
+	.db $88
+	.db $52
+	.db $84
+	.db $50
+	.db $48
+	.db $81
+	.db $4e
+	.db $00
+
+MusicDataSubSpace_Square1:
+	.db $84
+	.db $50
+	.db $58
+	.db $5a
+	.db $5e
+	.db $7e
+	.db $5a
+	.db $58
+	.db $52
+	.db $50
+	.db $52
+	.db $4e
+	.db $52
+	.db $7e
+	.db $50
+	.db $48
+	.db $4e
