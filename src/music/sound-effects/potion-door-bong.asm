7f8e469a7835fc97a770f70b40a4b70dc37a339a
TempoQuill <jicrapo@gmail.com>
2023-05-31 06:42:25 -0400

    Lamp sound should be longer

--- src/music/sound-effects/potion-door-bong.asm
+++ src/music/sound-effects/potion-door-bong.asm
@@ -7,5 +7,7 @@ SoundEffect1Data_PotionDoorBong:
 	.db $D0
 	.db $7E, $7E, $7E
 	.db $DF
-	.db $12, $10, $7E, $7E, $7E, $7E, $7E, $7E, $7E, $7E, $7E, $7E, $7E
+	.db $12, $10, $7E, $7E, $7E, $7E, $7E
+	.db $7E, $7E, $7E, $7E, $7E, $7E, $7E
+	.db $7E, $7E, $7E, $7E, $7E, $7E, $7E
 	.db $00
