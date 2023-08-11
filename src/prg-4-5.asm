6df94cfea708a5285099d73b8dfc21bf29c0a3b6
Tempo Quill <jicrapo@gmail.com>
2023-08-11 10:50:35 -0400

    Update file descriptions

--- src/prg-4-5.asm
+++ src/prg-4-5.asm
@@ -1647,17 +1647,17 @@ InstrumentRAMPointers:
 ; Frequency table for notes; standard between various Mario games
 .include "src/music/frequency-table.asm";
 
-; Note lengths for various BPM settings
+; Base note lengths and TRI_LINEAR parameters
 .include "src/music/note-lengths.asm";
 
-; Pointers to music segments
+; Pointers to music segments, padded to full length with $ff
 .include "src/music/music-part-pointers.asm"
 
-; Headers for songs (BPM, tracks to use, etc)
+; Headers for songs (BPM, tracks to use, etc), padded to full length with $ff
 .include "src/music/music-headers.asm"
 
-; More music pointers
+; Main music pointers
 .include "src/music/music-pointers.asm"
 
-; Channels active in the music
+; Channels active in the music (usually all 5)
 .include "src/music/music-channel-count.asm"
