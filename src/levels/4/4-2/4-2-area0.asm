79d5a290d0591fabe161667ca7ba9230361e34bb
Tempo Quill <jicrapo@gmail.com>
2023-09-02 08:53:47 -0400

    Do world 4

--- src/levels/4/4-2/4-2-area0.asm
+++ src/levels/4/4-2/4-2-area0.asm
@@ -4,7 +4,7 @@ LevelData_4_2_Area0:
 	; Level Header
 	;   pages (0-indexed), orientation, background palette, sprite palette, music,
 	;   AX-FX type, 3X-9X type, ground setting (0-31), ground type (0-7)
-	levelHeader 1, LevelDirection_Horizontal, 0, 0, LevelMusic_Overworld, 0, 0, $0a, $2
+	levelHeader 1, LevelDirection_Horizontal, 2, 1, LevelMusic_Underground, 0, 0, $0a, $2
 
 	.db $AC, $3C
 	.db $F0, $8C
